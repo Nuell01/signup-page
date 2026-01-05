78fecacefe11ca25f6079d3254415e1650f24f1f
Chinonso <nuellegbusunwa681@gmail.com>
2026-01-05 04:28:50 -1200

    made few changes on the background-color

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ myBtn.onclick = function(e){
 try{
  if(age < 18){
     alert("you must be 18yrs of age to enter this site")
+    return;
 }
-if(!mail.includes('@')){
+else if(!mail.includes('@')){
     window.alert('your email must include @ when filling your email');
     return;
 }
 
-else{
-    window.alert("Signed up sucessfully");
+else{ 
+    window.alert("Sign-in sucessfull");
 }
 }
 catch(error){
